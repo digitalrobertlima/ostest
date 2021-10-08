70483550b746d73502dd30ff2093d5e115fdf88c
Robert Lima <61129273+digitalrobertlima@users.noreply.github.com>
2021-10-08 16:41:14 -0300

    Criando arquivo com status do programa
    True or false;

---
+++ areiaDeGatos.js
@@ -0,0 +1,18 @@
+//PROGRAMA PARA LIMPEZA DE AREIA DE GATOS
+
+var statusPrograma = true;
+
+function main(){
+
+if (statusPrograma !== true){
+
+console.log('Programa Desativado');
+
+} else {
+
+console.log('Programa ligado');
+}
+
+}
+
+return main();

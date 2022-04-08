4cef269bf379e7279f5661781378e1f87783e123
Robert Lima <61129273+digitalrobertlima@users.noreply.github.com>
2022-04-08 16:17:25 -0300

    Update app2.js
    consertando algumas coisas no código

--- app2.js
+++ app2.js
@@ -19,25 +19,25 @@ function printVolume (volume) {
 
 function contarWhileBid(objeto, i, whileAmount, amountWhile) {
         if (whileAmount >= amountWhile) {
-                        let price = parseFloat(objeto.bids[i].price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });                                                                           console.log("Compra sugerida: " + green.bold(price) + " " + whileAmount + " BTC");                                                          } else {
-                }
+                let price = parseFloat(objeto.bids[i].price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                console.log("Compra sugerida: " + green.bold(price) + " " + whileAmount + " BTC");
+                
+        } else {}
 }
 
-function contarWhileAsk(objeto, i, whileAsk, amountWhile) {                                      if (whileAsk > amountWhile) {
-        let price = parseFloat(objeto.asks[i].price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        console.log('Venda sugerida: ' + red.bold(price) + " " + whileAsk + " BTC");
-} else {}
+function contarWhileAsk(objeto, i, whileAsk, amountWhile) {
+        if (whileAsk > amountWhile) {
+                let price = parseFloat(objeto.asks[i].price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                console.log('Venda sugerida: ' + red.bold(price) + " " + whileAsk + " BTC");
+                } else {}
 }
 
 function imprimeWhile(objeto) {
-
         let amountWhile = 0.1;
-
         for (let i = 0; i < 40; i++) {
                 let whileAmount = objeto.bids[i].amount;
                 contarWhileBid(objeto, i, whileAmount, amountWhile);
         }
-
         for (let i = 0; i < 40; i++) {
                 let whileAsk = objeto.asks[i].amount;
                 contarWhileAsk(objeto, i, whileAsk, amountWhile);
@@ -45,25 +45,20 @@ function imprimeWhile(objeto) {
 }
 
 function melhorPreco(objeto) {
-
-        let buy = parseFloat(objeto.bids[0].price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + " " + objeto.bids[0].amount + " BTC";                                      let sell = parseFloat(objeto.asks[0].price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + " " + objeto.asks[0].amount.toFixed(8) + " BTC";
+        let buy = parseFloat(objeto.bids[0].price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + " " + objeto.bids[0].amount + " BTC";
+        let sell = parseFloat(objeto.asks[0].price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + " " + objeto.asks[0].amount.toFixed(8) + " BTC";
         let melhorComprador = "\nMelhor pagador: " + green.bold(buy);
         let melhorVendedor = "Melhor vendedor: " + yellow.bold(sell);
-
         console.log(melhorComprador + "\n" + melhorVendedor + "\n");
 }
 
 function imprimeOpostos(maior, menor) {
-
-        console.log("Maior preço registrado em 24H: " + red(maior));;
-
+        console.log("Maior preço registrado em 24H: " + red(maior));
         console.log("Menor preço registrado em 24H: " + green(menor));
 }
 
 function variacao24horas(variacao) {
-
         let message = "Variação em 24H: ";
-
         if (variacao > 0) {
                 console.log(message + green.bold(variacao + "%"));
         } else if (variacao < 0) {
@@ -74,29 +69,29 @@ function variacao24horas(variacao) {
 }
 
 function imprimeForca(objeto) {
-
-        let somaCompras = 0;                          let somaVendas = 0;
-
+        let somaCompras = 0;
+        let somaVendas = 0;
+        
         //console.log(objeto);
-
+        
         for(let i = 0; i < 40; i++) {
                 somaCompras = somaCompras + objeto.bids[i].amount;
                 somaVendas = somaVendas + objeto.asks[i].amount;
         }
-
+        
         console.log("Força de compra(40 pessoas): " + green(somaCompras.toFixed(8)) + " BTC's");
         console.log("Força de venda(40 pessoas): " + red(somaVendas.toFixed(8)) + " BTC's");
 }
 
 function imprimeBook(objeto) {
-
+        
         imprimeForca(objeto);
         melhorPreco(objeto);
         imprimeWhile(objeto);
 }
 
 function imprimeTicker(objeto) {
-
+        
         let lastPrice = parseFloat(objeto.last).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
         let volume = parseFloat(objeto.vol);
         let variacao = parseFloat(objeto.var);
@@ -122,13 +117,11 @@ function definirObjeto(objeto) {
         if (tamanhoObjeto == 11) {
 
                 console.log(blue.bold('\nAguardando ticker...'));
-
                 imprimeTicker(objeto);
 
         } else if (tamanhoObjeto == 4) {
 
                 console.log(blue.bold('\nAguardando orderbook...\n'));
-
                 imprimeBook(objeto);
 
         } else {
@@ -137,6 +130,7 @@ function definirObjeto(objeto) {
 }
 
 function getJSON(url) {
+        
         const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
         const request = new XMLHttpRequest();
 
@@ -163,6 +157,8 @@ function getPrice(ticker, orderbook) {
 function main() {
         console.log(yellow('\nCarregando...'));
         getPrice(urlTicker, urlOrderbook);
+        
+        return
 }
 
 console.log(green('\nAbrindo API... Por favor aguarde!'));
